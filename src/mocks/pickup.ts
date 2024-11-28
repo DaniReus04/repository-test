c15d1b4f5ede79e1b89e748bcef3cde9969a5bda
Daniel C. <daniel_carvalho_amaroo@outlook.com>
2024-11-27 21:39:41 -0300

    fix: pickup request history mock order

--- src/mocks/pickup.ts
+++ src/mocks/pickup.ts
@@ -2,36 +2,14 @@ import { IPickupHistory } from '../interfaces/pickup';
 
 const mockPickupHistory: IPickupHistory[] = [
   {
-    id: 1,
+    id: 5,
     stationId: 1,
-    collectedAt: Number(new Date(2024, 10, 11)),
-    volume: 1780,
+    collectedAt: Number(new Date(2024, 10, 20)),
+    volume: 2080,
     storedWasteMaterials: [
-      { id: 1, volume: 550 },
-      { id: 4, volume: 750 },
-      { id: 7, volume: 480 },
-    ],
-  },
-  {
-    id: 2,
-    stationId: 2,
-    collectedAt: Number(new Date(2024, 10, 13)),
-    volume: 1680,
-    storedWasteMaterials: [
-      { id: 1, volume: 550 },
-      { id: 7, volume: 450 },
-      { id: 8, volume: 680 },
-    ],
-  },
-  {
-    id: 3,
-    stationId: 1,
-    collectedAt: Number(new Date(2024, 10, 14)),
-    volume: 1980,
-    storedWasteMaterials: [
-      { id: 1, volume: 550 },
-      { id: 4, volume: 450 },
-      { id: 7, volume: 980 },
+      { id: 1, volume: 150 },
+      { id: 4, volume: 150 },
+      { id: 2, volume: 1780 },
     ],
   },
   {
@@ -46,14 +24,37 @@ const mockPickupHistory: IPickupHistory[] = [
     ],
   },
   {
-    id: 5,
+    id: 3,
     stationId: 1,
-    collectedAt: Number(new Date(2024, 10, 20)),
-    volume: 2080,
+    collectedAt: Number(new Date(2024, 10, 14)),
+    volume: 1980,
     storedWasteMaterials: [
-      { id: 1, volume: 150 },
-      { id: 4, volume: 150 },
-      { id: 2, volume: 1780 },
+      { id: 1, volume: 550 },
+      { id: 4, volume: 450 },
+      { id: 7, volume: 980 },
+    ],
+  },
+  {
+    id: 2,
+    stationId: 2,
+    collectedAt: Number(new Date(2024, 10, 13)),
+    volume: 1680,
+    storedWasteMaterials: [
+      { id: 1, volume: 550 },
+      { id: 7, volume: 450 },
+      { id: 8, volume: 680 },
+    ],
+  },
+
+  {
+    id: 1,
+    stationId: 1,
+    collectedAt: Number(new Date(2024, 10, 11)),
+    volume: 1780,
+    storedWasteMaterials: [
+      { id: 1, volume: 550 },
+      { id: 4, volume: 750 },
+      { id: 7, volume: 480 },
     ],
   },
 ];
